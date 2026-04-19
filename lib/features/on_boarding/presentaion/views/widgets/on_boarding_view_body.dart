9de20e83d831f2326ae96ab1c2382865ba46d92d
Mostafa Tarek <moustafatarek013@gmail.com>
2026-04-19 14:14:03 +0200

    add logic part 1

--- lib/features/on_boarding/presentaion/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentaion/views/widgets/on_boarding_view_body.dart
@@ -1,8 +1,32 @@
 import 'package:e_commerce/features/on_boarding/presentaion/views/widgets/one_boarding_page_view.dart';
 import 'package:flutter/material.dart';
 
-class OnBoardingViewBody extends StatelessWidget {
+class OnBoardingViewBody extends StatefulWidget {
   const OnBoardingViewBody({super.key});
+
+  @override
+  State<OnBoardingViewBody> createState() => _OnBoardingViewBodyState();
+}
+
+class _OnBoardingViewBodyState extends State<OnBoardingViewBody> {
+  late PageController pageController;
+  var currentPage = 0;
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(() {
+      setState(() {
+        currentPage = pageController.page!.round();
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
