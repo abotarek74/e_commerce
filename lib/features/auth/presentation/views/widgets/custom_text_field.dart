283cdeb421c3f96e2cfdf96710a53a986f53e364
Mostafa Tarek <moustafatarek013@gmail.com>
2026-04-22 17:36:41 +0200

    create login UI

---
+++ lib/features/auth/presentation/views/widgets/custom_text_field.dart
@@ -0,0 +1,37 @@
+import 'package:e_commerce/core/utils/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    super.key,
+    required this.hintText,
+    required this.textInputType,
+    this.suffixIcon,
+  });
+  final String hintText;
+  final TextInputType textInputType;
+  final Widget? suffixIcon;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: textInputType,
+      decoration: InputDecoration(
+        suffixIcon: suffixIcon,
+        hintText: hintText,
+        hintStyle: TextStyles.bold13.copyWith(color: Color(0XFF949D9E)),
+        filled: true,
+        fillColor: Color(0XFFE6E9E9),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(4),
+      borderSide: BorderSide(color: Color(0XFFE6E9E9), width: 1),
+    );
+  }
+}
--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce/constants.dart';
+import 'package:e_commerce/features/auth/presentation/views/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -5,6 +7,28 @@ class LoginViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: KHhorizontalPadding),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            CustomTextFormField(
+              hintText: 'البريد الالكتروني',
+              textInputType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              suffixIcon: Icon(
+                Icons.visibility_off_outlined,
+                color: Color(0XFF949D9E),
+              ),
+              hintText: 'كلمة المرور',
+              textInputType: TextInputType.visiblePassword,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
